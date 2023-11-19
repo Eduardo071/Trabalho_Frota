8137833e026431d15e7e19716bfebc4685503123
Delta <ti22mau@gmail.com>
2023-11-19 15:35:04 -0300

    CORREÇÃO

--- fornecedor.h
+++ fornecedor.h
@@ -1,81 +1,78 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Fornecedor
-{
-    char razao_social[50];
-    char fantasia[100];
-    char cnpj[15];
-    char inc_estadual[50];
-    char inc_municipal[50];
-    char cep[8];
-    char endereco[100];
-    char numero[6];
-    char complemento[50];
-    char bairro[50];
-    char cidade[50];
-    char uf[3];
-    char telefone[15];
-    char email[100];
-    char data_fund[8];
-    char site[30];
+struct Fornecedor {
+  char razao_social[50];
+  char fantasia[100];
+  char cnpj[15];
+  char inc_estadual[50];
+  char inc_municipal[50];
+  char cep[8];
+  char endereco[100];
+  char numero[6];
+  char complemento[50];
+  char bairro[50];
+  char cidade[50];
+  char uf[3];
+  char telefone[15];
+  char email[100];
+  char data_fund[8];
+  char site[30];
 };
 
-void adicionarFornecedor()
-{
-    struct Fornecedor novoFornecedor;
-    printf("<<< Cadastro de Fornecedor >>>\n");
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
-    printf("Razão Social: ");
-    fgets(novoFornecedor.razao_social, sizeof(novoFornecedor.razao_social),
-          stdin);
+void adicionarFornecedor() {
+  struct Fornecedor novoFornecedor;
 
-    printf("Fantasia: ");
-    fgets(novoFornecedor.fantasia, sizeof(novoFornecedor.fantasia), stdin);
+  printf("<<< Cadastro de Fornecedor >>>\n");
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
+  printf("Razão Social: ");
+  fgets(novoFornecedor.razao_social, sizeof(novoFornecedor.razao_social),
+        stdin);
 
-    printf("CNPJ: ");
-    fgets(novoFornecedor.cnpj, sizeof(novoFornecedor.cnpj), stdin);
+  printf("Fantasia: ");
+  fgets(novoFornecedor.fantasia, sizeof(novoFornecedor.fantasia), stdin);
 
-    printf("Inscrição Estadual: ");
-    fgets(novoFornecedor.inc_estadual, sizeof(novoFornecedor.inc_estadual),
-          stdin);
+  printf("CNPJ: ");
+  fgets(novoFornecedor.cnpj, sizeof(novoFornecedor.cnpj), stdin);
 
-    printf("CEP: ");
-    fgets(novoFornecedor.cep, sizeof(novoFornecedor.cep), stdin);
+  printf("Inscrição Estadual: ");
+  fgets(novoFornecedor.inc_estadual, sizeof(novoFornecedor.inc_estadual),
+        stdin);
 
-    printf("Endereço: ");
-    fgets(novoFornecedor.endereco, sizeof(novoFornecedor.endereco), stdin);
+  printf("CEP: ");
+  fgets(novoFornecedor.cep, sizeof(novoFornecedor.cep), stdin);
 
-    printf("Número: ");
-    fgets(novoFornecedor.numero, sizeof(novoFornecedor.numero), stdin);
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
-    printf("Complemento: ");
-    fgets(novoFornecedor.complemento, sizeof(novoFornecedor.complemento), stdin);
+  printf("Endereço: ");
+  fgets(novoFornecedor.endereco, sizeof(novoFornecedor.endereco), stdin);
 
-    printf("Bairro: ");
-    fgets(novoFornecedor.bairro, sizeof(novoFornecedor.bairro), stdin);
+  printf("Número: ");
+  fgets(novoFornecedor.numero, sizeof(novoFornecedor.numero), stdin);
 
-    printf("Cidade: ");
-    fgets(novoFornecedor.cidade, sizeof(novoFornecedor.cidade), stdin);
+  printf("Complemento: ");
+  fgets(novoFornecedor.complemento, sizeof(novoFornecedor.complemento), stdin);
 
-    printf("UF: ");
-    fgets(novoFornecedor.uf, sizeof(novoFornecedor.uf), stdin);
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
-    printf("Telefone: ");
-    fgets(novoFornecedor.telefone, sizeof(novoFornecedor.telefone), stdin);
+  printf("Bairro: ");
+  fgets(novoFornecedor.bairro, sizeof(novoFornecedor.bairro), stdin);
 
-    printf("E-mail: ");
-    fgets(novoFornecedor.email, sizeof(novoFornecedor.email), stdin);
+  printf("Cidade: ");
+  fgets(novoFornecedor.cidade, sizeof(novoFornecedor.cidade), stdin);
 
-    printf("Data de Fundação: ");
-    fgets(novoFornecedor.data_fund, sizeof(novoFornecedor.data_fund), stdin);
+  printf("UF: ");
+  fgets(novoFornecedor.uf, sizeof(novoFornecedor.uf), stdin);
+  
+  printf("Telefone: ");
+  fgets(novoFornecedor.telefone, sizeof(novoFornecedor.telefone), stdin);
 
-    printf("Site: ");
-    fgets(novoFornecedor.site, sizeof(novoFornecedor.site), stdin);
+  printf("E-mail: ");
+  fgets(novoFornecedor.email, sizeof(novoFornecedor.email), stdin);
 
-    printf("Fornecedor adicionado com sucesso!\n");
+  printf("Data de Fundação: ");
+  fgets(novoFornecedor.data_fund, sizeof(novoFornecedor.data_fund), stdin);
+
+  printf("Site: ");
+  fgets(novoFornecedor.site, sizeof(novoFornecedor.site), stdin);
+
+  printf("Fornecedor adicionado com sucesso!\n");
 }
